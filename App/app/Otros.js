e95b1c55304c42aded546d21ec31da51f160831b
Silvina Rivarola <Sil_vina@Silvina.(none)>
2012-03-14 11:34:47 -0300

    third commit

--- App/app/Otros.js
+++ App/app/Otros.js
@@ -1,26 +1,30 @@
-﻿   $(document).ready(function() {
-        alert("La pagina termino de cargarse");
-     });
+$(document).ready(function() {
+     alert("La pagina termino de cargarse");
+ });
 	 
-	 //document.getElementById('Alias').focus();
-	$(document).ready(function() {
-		$('#Alias').focus();
-	 });
+ //document.getElementById('Alias').focus();
+$(document).ready(function() {
+    $('#Alias').focus();
+ });
 	 
-	 $(document).ready(function () {
-                $ ("#Boton1").click(function (evento) {
-   				     evento.preventDefault();
-					var param = $("#Alias").val();
-					url = "api/dispatcher.php";
-        			$.post(url, {service: 'welcome.hello', params: {"name":""+ param+""}},
-									function(data){
-										$("#response").html(data);
-							});
-					$("#Alias").val('');
-					$("#Alias").focus();
-					});
-			
-			});
-		
-			
-				
+ $(document).ready(function () {
+       $ ("#Boton1").click(function (evento) {
+                evento.preventDefault();
+                var param = $("#Alias").val();
+		url = "api/dispatcher.php";
+        	$.post(url, {service: 'welcome.hello', params: {"name":""+ param+""}},
+					 function(data){
+                                              $("#response").html(data);
+                                               changeColor();
+                           });
+                $("#Alias").val('');
+                $("#Alias").focus();
+	});
+    });
+	
+function changeColor(){
+       element = document.getElementById("response");
+       element.style.backgroundColor = '#00FFFF';
+   };
+       
+  
