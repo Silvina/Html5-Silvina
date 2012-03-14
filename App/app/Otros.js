0d386cb6d603c15876b0b790110c814f6579d2a2
Silvina Rivarola <Sil_vina@Silvina.(none)>
2012-03-14 10:16:16 -0300

    second commit

---
+++ App/app/Otros.js
@@ -0,0 +1,26 @@
+﻿   $(document).ready(function() {
+        alert("La pagina termino de cargarse");
+     });
+	 
+	 //document.getElementById('Alias').focus();
+	$(document).ready(function() {
+		$('#Alias').focus();
+	 });
+	 
+	 $(document).ready(function () {
+                $ ("#Boton1").click(function (evento) {
+   				     evento.preventDefault();
+					var param = $("#Alias").val();
+					url = "api/dispatcher.php";
+        			$.post(url, {service: 'welcome.hello', params: {"name":""+ param+""}},
+									function(data){
+										$("#response").html(data);
+							});
+					$("#Alias").val('');
+					$("#Alias").focus();
+					});
+			
+			});
+		
+			
+				
